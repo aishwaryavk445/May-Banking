a6fe1c671fa77cce96435e598c126b3d3019a525
aishwaryavk445 <141556221+aishwaryavk445@users.noreply.github.com>
2023-10-15 22:03:12 +0530

    Updated provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -1,17 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
-}
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+variable "region" {
+  default = "us-west-2"
 }
 
+provider "aws" {
+  region = var.region
+}
